4af0355ab5720ec4ba7f24e1b191a493e6a69392
Foivos Zakkak <fzakkak@redhat.com>
2020-09-03 13:26:44 +0300

    Bump version in suites

--- regex/mx.regex/suite.py
+++ regex/mx.regex/suite.py
@@ -43,8 +43,8 @@ suite = {
 
   "name" : "regex",
 
-  "version" : "20.1.0.2",
-  "release" : True,
+  "version" : "20.1.0.3",
+  "release" : False,
   "groupId" : "org.graalvm.regex",
   "url" : "http://www.graalvm.org/",
   "developer" : {
