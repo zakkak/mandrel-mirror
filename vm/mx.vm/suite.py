4cb0c1b36841186b0744e7341a00a4a1d679894b
Foivos Zakkak <fzakkak@redhat.com>
2020-09-23 12:55:08 +0300

    Mark suites for 20.1.0.3.Final release

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -2,7 +2,7 @@ suite = {
     "name": "vm",
     "version" : "20.1.0.3",
     "mxversion" : "5.256.0",
-    "release" : False,
+    "release" : True,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
