c5a47f03be9722163779195cfb12ce1f39e76941
Foivos Zakkak <fzakkak@redhat.com>
2020-10-13 01:44:46 +0300

    Unmark suites and bump version to 20.1.0.4

--- sdk/mx.sdk/suite.py
+++ sdk/mx.sdk/suite.py
@@ -41,8 +41,8 @@
 suite = {
   "mxversion" : "5.251.0",
   "name" : "sdk",
-  "version" : "20.1.0.3",
-  "release" : True,
+  "version" : "20.1.0.4",
+  "release" : False,
   "sourceinprojectwhitelist" : [],
   "url" : "https://github.com/oracle/graal",
   "groupId" : "org.graalvm.sdk",
